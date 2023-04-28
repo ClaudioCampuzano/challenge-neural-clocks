3b9fd172f430041feb649a6f6c3c195ab4b75153
Claudio <claudio.campuzano.b@gmail.com>
2023-04-28 12:15:29 -0400

    feat(page): add logic to manage the timers

--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,115 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+'use client';
+import { AvailableStates, useStore } from '@/store';
+import { SelectorBar } from '@/molecules/SelectorBar';
+import { Button } from '@/atoms/Button';
+import { useRef, useState } from 'react';
+import { ModalConfiguration } from '@/molecules/ModalConfiguration';
+import { CircularProgressBar, CircularBarHandle } from '@/atoms/CircularProgressBar';
+import { formatDate } from '@/utils/formatDate';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  const {
+    currentState,
+    pomodoroTimeMinutes,
+    shortPauseTimeMinutes,
+    longPauseTimeMinutes,
+    setTimeToTimer,
+    setState,
+  } = useStore();
+  const timerBarRef = useRef<CircularBarHandle>(null);
+  const [isModalConfigurationOpen, setIsModalConfigurationOpen] = useState(false);
+  const [isTimerRun, setIsTimerRun] = useState(false);
 
-export default function Home() {
+  const objectTimes: Record<string, number> = {
+    pomodoro: pomodoroTimeMinutes * 60_000,
+    shortPause: shortPauseTimeMinutes * 60_000,
+    longPause: longPauseTimeMinutes * 60_000,
+  };
+
+  const stateMachine: Record<string, AvailableStates> = {
+    pomodoro: 'shortPause',
+    shortPause: 'longPause',
+    longPause: 'pomodoro',
+  };
+
+  const onTimerEnd = () => {
+    const audio = new Audio('/alarm.mp3');
+    audio.play();
+    setState(stateMachine[currentState]);
+
+    setIsTimerRun(false);
+  };
+
+  const labelTimer = formatDate(
+    new Date(1000 * Math.max(Math.round(objectTimes[currentState] / 1000), 0)),
+    'es',
+    { minute: 'numeric', second: 'numeric' },
+  );
+
+  const rebootTimer = () => {
+    setIsTimerRun(false);
+    timerBarRef.current?.start(0);
+  };
+
+  const handleButton = () => {
+    if (isTimerRun) {
+      setIsTimerRun(false);
+      rebootTimer();
+    } else {
+      setIsTimerRun(true);
+      timerBarRef.current?.start(objectTimes[currentState]);
+    }
+  };
+
+  const handleNextStepButton = () => {
+    rebootTimer();
+    setState(stateMachine[currentState]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center m-5 space-y-3">
+      <ModalConfiguration
+        isOpen={isModalConfigurationOpen}
+        onClose={() => setIsModalConfigurationOpen(false)}
+        currentValues={{
+          pomodoro: pomodoroTimeMinutes,
+          shortPause: shortPauseTimeMinutes,
+          longPause: longPauseTimeMinutes,
+        }}
+        setValue={setTimeToTimer}
+      />
+      <div className="flex flex-col items-center space-y-3 my-40 p-8 border border-solid rounded-lg">
+        <div className="flex flex-row bg-primary-90 border rounded-lg p-3 space-x-24">
+          <div className="text-neutral-45 text-xl">NeuralClocks</div>
+          <Button iconName="SettingIcon" onClick={() => setIsModalConfigurationOpen(true)}>
+            Configuración
+          </Button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <SelectorBar currentState={currentState} setState={setState} isTimerRun={isTimerRun} />
+        <div className="w-80">
+          <CircularProgressBar
+            shouldShowTimer={isTimerRun}
+            onTimerEnd={onTimerEnd}
+            ref={timerBarRef}
+          />
+          {!isTimerRun ? (
+            <div className="mt-2 text-lg text-center text-neutral-45">
+              <span className="font-bold">{labelTimer}</span>
+            </div>
+          ) : (
+            false
+          )}
+        </div>
+        <div className="flex flex-row bg-primary-90 border rounded-lg p-3 space-x-24 w-96 justify-between">
+          <Button onClick={handleButton}>{isTimerRun ? 'Reiniciar' : 'Comenzar'}</Button>
+          <Button iconName="NextStepIcon" onClick={handleNextStepButton}>
+            Siguiente
+          </Button>
+        </div>
+        <Button href={'https://blog.hubspot.es/sales/tecnica-pomodoro'}>Que es pomodoro</Button>
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
